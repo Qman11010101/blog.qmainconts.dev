3ae211e53e7cbf5f81d070b6f62d81a3de3828b7
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2022-11-20 03:29:25 +0900

    wip

---
+++ assets/js/categories.js
@@ -0,0 +1,15 @@
+async function getCategory() {
+    const categories = await (await fetch("/category.json")).json();
+    const categoryDisplay = document.getElementById("categorydisplay");
+    for (let i = 0; i < categories.length; i++) {
+        let ID = categories[i].id;
+        let name = categories[i].name;
+        let tagBlock = categoryDisplay.appendChild(document.createElement("div"));
+        tagBlock.className = "tag";
+        let link = tagBlock.appendChild(document.createElement("a"));
+        link.href = "/articles/category/" + ID;
+        tagBlock.innerText += name;
+    }
+}
+
+getCategory();
